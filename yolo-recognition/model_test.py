40ba5327e689e4c7922b33bde6e4de180d0e0209
Cole Lewis <cole.g.lewis@gmail.com>
2022-11-01 15:45:24 -0400

    Cleaning old files and adding model evaulation IOU file

--- yolo-recognition/model_test.py
+++ yolo-recognition/model_test.py
@@ -9,13 +9,6 @@ import cv2, os, random
 
 dataset_types = ['train', 'test', 'validation']
 
-def generate_random_file_path(): # used to pick a random image and test with
-    root = f'data/Dataset/{random.choice(dataset_types)}/{random.choice(TRAIN_CLASSES)}/'
-    suffix = random.choice([file for file in os.listdir(root) if file.endswith('.jpg')])
-    print(suffix)
-    print('Path: ' + f'{root}{suffix}')
-    return f'{root}{suffix}'
-
 test_image_path = 'data/Dataset/train/Human_hand/7017da3aa12bbfe2.jpg'
 
 model = Load_Yolo_model()
